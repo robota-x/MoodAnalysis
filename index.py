0a41f942ef8725b88ca3e512957a6e48a78bf054
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-03-02 17:12:07 +0000

    update lambda response to print table data, fix template errors

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 import json
 import datetime
-
+import boto3
 
 def handler(event, context):
+    votes_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'secret': votes_table.scan()
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
